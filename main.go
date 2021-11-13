e943ef8cc8ef1608964de5ee1d749789738a15e2
whaleshark <whalesong0622@163.com>
2021-11-13 19:42:08 +0800

    add net

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"PDFS-Server/api"
-	"PDFS-Server/common"
 	"fmt"
+	"io"
+	"net"
 	"os"
-	"strings"
 )
 
-func main() {
+/*func main() {
 	args := os.Args
 	if len(args) == 1{
 		println("Please input your operation")
@@ -41,5 +40,73 @@ func main() {
 	}else{
 		fmt.Println("Input args error")
 		common.Help()
+	}
+}*/
+func revFile(fileName string, conn net.Conn) {
+	defer conn.Close()
+	fs, err := os.Create(fileName)
+	defer fs.Close()
+	if err != nil {
+		fmt.Println("os.Create err =", err)
+		return
+	}
+
+	// 拿到数据
+	buf := make([]byte, 1024*10)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("conn.Read err =", err)
+			if err == io.EOF {
+				fmt.Println("文件结束了", err)
+			}
+			return
+		}
+		if n == 0 {
+			fmt.Println("文件结束了", err)
+			return
+		}
+		fs.Write(buf[:n])
+
+	}
+}
+func main() {
+	// 创建一个服务器
+	Server, err := net.Listen("tcp", "127.0.0.1:8000")
+	if err != nil {
+		fmt.Println("net.Listen err =", err)
+		return
+	}
+	defer Server.Close()
+	// 接受文件名
+	for {
+		conn, err := Server.Accept()
+		defer conn.Close()
+		if err != nil {
+			fmt.Println("Server.Accept err =", err)
+			return
+		}
+		buf := make([]byte, 1024)
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("conn.Read err =", err)
+			return
+		}
+		op := string(buf[:n])
+		// 返回ok
+		conn.Write([]byte("ok"))
+
+		if op == "1" { //上传文件
+			n, err := conn.Read(buf)
+			if err != nil {
+				fmt.Println("conn.Read err =", err)
+			}
+			fileName := string(buf[:n])
+			conn.Write([]byte("ok"))
+			revFile(fileName, conn)
+		} else {
+
+		}
+
 	}
 }
